47586ad01de566c9ce173c4fb47d8ab8e4020c8c
Max Radaev <m.radaev@geoscan.ru>
2025-02-12 10:27:40 +0300

    done Homework03

--- src/main/java/ru/otus/java/basic/homeworks/hw03/Homework03.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw03/Homework03.java
@@ -5,12 +5,31 @@ import java.util.Scanner;
 public class Homework03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        scanner.nextInt();
-        greetings();
-        checkSign(1, 2, 3);
-        selectColor();
-        compareNumbers();
-        addOrSubtractAndPrint(1, 3, true);
+        System.out.println("Введите число от 1 до 5");
+        int initNum = scanner.nextInt();
+        switch (initNum) {
+            case 1:
+                greetings();
+                break;
+            case 2:
+                double randomNum = (Math.random() * 100) + 1;
+                double randomNum2 = (Math.random() * 100) + 1;
+                double randomNum3 = (Math.random() * 100) + 1;
+                checkSign((int) randomNum, (int) randomNum2, (int) randomNum3);
+                break;
+            case 3:
+                selectColor();
+                break;
+            case 4:
+                compareNumbers();
+                break;
+            case 5:
+                double initValue = (Math.random() * 100) + 1;
+                double delta = (Math.random() * 100) + 1;
+                boolean increment = ((int) (Math.random() * 100)) % 2 == 1;
+                addOrSubtractAndPrint((int) initValue, (int) delta, increment);
+                break;
+        }
     }
 
     public static void greetings() {
@@ -27,7 +46,7 @@ public class Homework03 {
     }
 
     public static void selectColor() {
-        int data = 0;
+        double data = (Math.random() * 30) + 1;
         if (data <= 10) {
             System.out.println("Красный");
         } else if (data > 10 && data <= 20) {
@@ -38,8 +57,8 @@ public class Homework03 {
     }
 
     public static void compareNumbers() {
-        int a = 1;
-        int b = 2;
+        int a = (int) (Math.random() * 100) + 1;
+        int b = (int) (Math.random() * 100) + 1;
         if (a >= b) {
             System.out.println("a >= b");
         } else {
