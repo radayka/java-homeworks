40b86736592f4e845778d99c3fb144aa8af5e224
Max Radaev <m.radaev@geoscan.ru>
2025-02-12 16:37:09 +0300

    improve code

--- src/main/java/ru/otus/java/basic/homeworks/hw03/Homework03.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw03/Homework03.java
@@ -4,36 +4,43 @@ import java.util.Scanner;
 
 public class Homework03 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число от 1 до 5");
-        int initNum = scanner.nextInt();
-        switch (initNum) {
-            case 1:
-                greetings();
-                break;
-            case 2:
-                double randomNum = (Math.random() * 100) + 1;
-                double randomNum2 = (Math.random() * 100) + 1;
-                double randomNum3 = (Math.random() * 100) + 1;
-                checkSign((int) randomNum, (int) randomNum2, (int) randomNum3);
-                break;
-            case 3:
-                selectColor();
-                break;
-            case 4:
-                compareNumbers();
-                break;
-            case 5:
-                double initValue = (Math.random() * 100) + 1;
-                double delta = (Math.random() * 100) + 1;
-                boolean increment = ((int) (Math.random() * 100)) % 2 == 1;
-                addOrSubtractAndPrint((int) initValue, (int) delta, increment);
-                break;
+        int n = 0;
+        while (n < 5) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите число от 1 до 5");
+            int initNum = scanner.nextInt();
+            switch (initNum) {
+                case 1:
+                    greetings();
+                    break;
+                case 2:
+                    double randomNum = (Math.random() * 100) + 1;
+                    double randomNum2 = (Math.random() * 100) + 1;
+                    double randomNum3 = (Math.random() * 100) + 1;
+                    checkSign((int) randomNum, (int) randomNum2, (int) randomNum3);
+                    break;
+                case 3:
+                    selectColor();
+                    break;
+                case 4:
+                    compareNumbers();
+                    break;
+                case 5:
+                    double initValue = (Math.random() * 100) + 1;
+                    double delta = (Math.random() * 100) + 1;
+                    boolean increment = ((int) (Math.random() * 100)) % 2 == 1;
+                    addOrSubtractAndPrint((int) initValue, (int) delta, increment);
+                    break;
+                default:
+                    System.out.println("Вы ввели число вне диапазона");
+                    break;
+            }
+            n++;
         }
     }
 
     public static void greetings() {
-        System.out.print("Hello\nWorld\nfrom\nJava");
+        System.out.println("Hello\nWorld\nfrom\nJava");
     }
 
     public static void checkSign(int a, int b, int c) {
