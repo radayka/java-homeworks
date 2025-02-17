2d672d1799d95931bc5d77a2a366822fd678ee21
Max Radaev <m.radaev@geoscan.ru>
2025-02-17 17:21:54 +0300

    done 1 step

--- src/main/java/ru/otus/java/basic/homeworks/hw07/homework07.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw07/homework07.java
@@ -1,22 +1,34 @@
 package ru.otus.java.basic.homeworks.hw07;
 
+import java.util.Arrays;
+
 public class homework07 {
     public static void main(String[] args) {
-        int[][] array = {{5,7,3,17},
-                         {7,0,1,12},
-                         {8,1,2,3}};
+        int[][] array = {{0, 0, 0, 17},
+                {0, 0, 0, 12},
+                {0, 1, 0, 0}};
         sumOfPositiveElements(array);
+        printSquare(4);
     }
 
     public static void sumOfPositiveElements(int[][] array) {
         int count = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
                 if (array[i][j] > 0) {
                     count += array[i][j];
                 }
             }
         }
         System.out.println(count);
+    }
+
+    public static void printSquare(int size) {
+        int[][] square = new int[size][size];
+        for (int i = 0; i < square.length; i++) {
+            for (int j = 0; j < square[0].length; j++) {
+                System.out.println();
+            }
+        }
     }
 }
