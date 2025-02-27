b771127146d8f66d77d167ca9817a4e5e705a88c
Max Radaev <m.radaev@geoscan.ru>
2025-02-27 11:32:48 +0300

    Continue homework

--- src/main/java/ru/otus/java/basic/homeworks/homework_03_01/dop_homework/Box.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework_03_01/dop_homework/Box.java
@@ -1,52 +1,53 @@
 package ru.otus.java.basic.homeworks.homework_03_01.dop_homework;
 
+import java.util.Arrays;
+
 public class Box {
 
     private boolean isOpen;
+
     private final int size;
-    private final String color;
+    private String color;
+    private boolean hasItem = false;
+    private String[] itemL = new String[1];
+
 
     public Box(String color, int size) {
         this.color = color;
         this.size = size;
-        this.isOpen = false;
     }
 
-    public void openBox(Box box) {
+    public void open(Box box) {
         box.isOpen = true;
         System.out.println("Коробка открыта");
     }
 
-    public void closeBox(Box box) {
+    public void close(Box box) {
         box.isOpen = false;
         System.out.println("Коробка закрыта");
     }
 
-    public void printBox(Box box) {
+    public void info(Box box) {
         System.out.println("Цвет коробки: " + box.color);
         System.out.println("Размер коробки: " + box.size);
-        System.out.println();
+        System.out.println("В коробке есть: " + box.itemL[0]);
     }
 
-    public static void addItem(Box box, String item) {
-        if (box.isOpen) {
-            String[] list = new String[box.size];
-            for (int i = 0; i < list.length; i++) {
-                if (list[i].isBlank()) {
-                    list[i] = item;
-                }
+    public void addItem(Box box, String item) {
+        if (!box.hasItem) {
+            if (box.isOpen) {
+                Arrays.fill(itemL, item);
             }
         }
-        System.out.println("Предмет " + item + " добавлен");
     }
 
-//    public static void deleteItem(Box box, String item) {
-//        if (!box.isOpen) {
-//            for (int i = 0; i < box.size; i++) {
-//                if()
-//            }
-//        }
-//    }
+    public void deleteItem(Box box) {
+        if(box.hasItem) {
+            if (box.isOpen) {
+                Arrays.fill(itemL, "пустота");
+            }
+        }
+    }
 
     public boolean isOpen() {
         return isOpen;
@@ -54,5 +55,33 @@ public class Box {
 
     public void setOpen(boolean open) {
         isOpen = open;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isHasItem() {
+        return hasItem;
+    }
+
+    public void setHasItem(boolean hasItem) {
+        this.hasItem = hasItem;
+    }
+
+    public String[] getItemL() {
+        return itemL;
+    }
+
+    public void setItemL(String[] itemL) {
+        this.itemL = itemL;
     }
 }
--- src/main/java/ru/otus/java/basic/homeworks/homework_03_01/dop_homework/Main.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework_03_01/dop_homework/Main.java
@@ -3,7 +3,11 @@ package ru.otus.java.basic.homeworks.homework_03_01.dop_homework;
 public class Main {
     public static void main(String[] args) {
         Box box = new Box("Красный",5);
-        Box.addItem(box,"Меч");
-        box.printBox(box);
+        box.open(box);
+        box.addItem(box,"Меч");
+        box.info(box);
+
+        box.deleteItem(box);
+        box.info(box);
     }
 }
